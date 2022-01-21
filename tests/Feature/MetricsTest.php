df0d9eb5ef9f305450335b0149e40d4cbfdf00bd
Paweł Chołaj <pawel.cholaj@escola.pl>
2022-01-21 17:13:17 +0100

    Improved code coverage (#21)
    * Improved code coverage

--- tests/Feature/MetricsTest.php
+++ tests/Feature/MetricsTest.php
@@ -58,6 +58,9 @@ class MetricsTest extends TestCase
         $this->assertEquals($course->title, $measurement->label);
         $this->assertEquals($report->getKey(), $measurement->report->getKey());
         $this->assertEquals($course->getKey(), $measurement->measurable->getKey());
+
+        $this->assertEquals(class_exists(\EscolaLms\Courses\EscolaLmsCourseServiceProvider::class), CoursesSecondsSpentMetric::make()->requiredPackageInstalled());
+        $this->assertIsString(CoursesSecondsSpentMetric::make()->requiredPackage());
     }
 
     public function testCoursesPopularityMetric()
@@ -88,6 +91,9 @@ class MetricsTest extends TestCase
         $this->assertEquals($course->title, $measurement->label);
         $this->assertEquals($report->getKey(), $measurement->report->getKey());
         $this->assertEquals($course->getKey(), $measurement->measurable->getKey());
+
+        $this->assertEquals(class_exists(\EscolaLms\Courses\EscolaLmsCourseServiceProvider::class), CoursesPopularityMetric::make()->requiredPackageInstalled());
+        $this->assertIsString(CoursesPopularityMetric::make()->requiredPackage());
     }
 
     public function testTutorsPopularityMetric()
@@ -127,6 +133,9 @@ class MetricsTest extends TestCase
         $this->assertEquals($tutor->email, $measurement->label);
         $this->assertEquals($report->getKey(), $measurement->report->getKey());
         $this->assertEquals($tutor->getKey(), $measurement->measurable->getKey());
+
+        $this->assertEquals(class_exists(\EscolaLms\Courses\EscolaLmsCourseServiceProvider::class), TutorsPopularityMetric::make()->requiredPackageInstalled());
+        $this->assertIsString(TutorsPopularityMetric::make()->requiredPackage());
     }
 
     public function testCourseMoneySpentMetric()
@@ -158,5 +167,8 @@ class MetricsTest extends TestCase
         $this->assertEquals($course->title, $measurement->label);
         $this->assertEquals($report->getKey(), $measurement->report->getKey());
         $this->assertEquals($course->getKey(), $measurement->measurable->getKey());
+
+        $this->assertEquals(class_exists(\EscolaLms\Courses\EscolaLmsCourseServiceProvider::class), CoursesMoneySpentMetric::make()->requiredPackageInstalled());
+        $this->assertIsString(CoursesMoneySpentMetric::make()->requiredPackage());
     }
 }
