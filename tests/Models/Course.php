b64506c04d71ba1959fbe59117961444a5c362c2
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-06-02 14:58:28 +0200

    Repair declare method attach in tests

--- tests/Models/Course.php
+++ tests/Models/Course.php
@@ -4,6 +4,7 @@ namespace EscolaLms\Reports\Tests\Models;
 
 use EscolaLms\Cart\Contracts\Productable;
 use EscolaLms\Cart\Contracts\ProductableTrait;
+use EscolaLms\Cart\Models\Product;
 use EscolaLms\Core\Models\User;
 use EscolaLms\Courses\Models\Course as BaseCourse;
 
@@ -11,7 +12,7 @@ class Course extends BaseCourse implements Productable
 {
     use ProductableTrait;
 
-    public function attachToUser(User $user, int $quantity = 1): void
+    public function attachToUser(User $user, int $quantity = 1, ?Product $product = null): void
     {
         $this->users()->syncWithoutDetaching($user->getKey());
     }
